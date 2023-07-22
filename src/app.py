35a4c3a6f51003d97230a069a812867c6e1ec678
behlock <behlocks@gmail.com>
2023-07-22 18:43:55 +0100

    Allow CORS

--- src/app.py
+++ src/app.py
@@ -1,12 +1,22 @@
 from starlette.applications import Starlette
 from starlette_graphene3 import GraphQLApp, make_graphiql_handler
+from starlette.applications import Starlette
+from starlette.middleware.cors import CORSMiddleware
 import graphene
 
 from nightsservice.api.graphql.query import Query
 
 if __name__ == "__main__":
-    app = Starlette()
     schema = graphene.Schema(query=Query)
+
+    app = Starlette()
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
     app.mount("/", GraphQLApp(schema, on_get=make_graphiql_handler()))
 
